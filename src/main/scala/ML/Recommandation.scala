bbd40998ef4108d72a06623558027f89ca6cb04f
San Luo <luohui20001@sina.com>
2016-07-13 03:07:41 +0800

    先用了一个比较草的特征向量和标签列作实验， 用逻辑回归训练模型的时候遇到异常org.apache.spark.SparkException: Classification labels should be in {0 to 5 Found 5616781 invalid labels.但是这个label都是0~5之间的double值[1.0] [3.5] [4.5] [0.5] [3.0] [1.5] [5.0] [2.5] [4.0] [2.0] 。很奇怪为什么会报这样的异常

--- src/main/scala/ML/Recommandation.scala
+++ src/main/scala/ML/Recommandation.scala
@@ -1,6 +1,7 @@
 package main.scala.ML
 
 import org.apache.spark._
+import org.apache.spark.sql._
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.param.ParamMap
 import org.apache.spark.mllib.linalg.{ Vector, Vectors }
@@ -17,18 +18,26 @@ object Recommandation {
     hc.sql("cache table ratings")
     hc.sql("cache table links")
     hc.sql("cache table tags")
-    hc.sql("alter table ratings change timestamp time double")
-    
-//    val ratings = hc.sql("select * from ratings")
-    val ratings = hc.sql("select * from ratings group by userId,movieid,rating,timestamp")
+
+    //    val ratings = hc.sql("select * from ratings")
+    val ratings = hc.sql("select * from ratings")
     ratings.printSchema()
     ratings.show()
 
-    val lr = new LogisticRegression().setFeaturesCol("movieId")
+    val vectors = org.apache.spark.sql.functions.udf((movieId: Int, rating: Float) =>
+      Vectors.dense(movieId, rating))
+    val label = org.apache.spark.sql.functions.udf((rating: Float) =>
+      rating.toDouble)
+    val newratings = ratings.withColumn("featureV", vectors(ratings.col("movieId"), ratings.col("rating"))).withColumn("label", label(ratings.col("rating")))
+    newratings.printSchema()
+    newratings.registerTempTable("newratings")
+    hc.sql("select distinct(label) from newratings").collect.foreach(println)
+
+    val lr = new LogisticRegression().setFeaturesCol("featureV").setLabelCol("label")
     println("LogisticRegression parameters:\n" + lr.explainParams() + "\n")
     lr.setMaxIter(10).setRegParam(0.01)
-    
-    val model1 = lr.fit(ratings)
+
+    val model1 = lr.fit(newratings)
     println("Model 1 was fit using parameters: " + model1.parent.extractParamMap)
 
   }
