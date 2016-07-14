549e1b0680caf1904467ea92d9fc26e85fe60ee6
yuwangtian <yuwangtian@aliyun.com>
2016-07-14 18:02:52 +0800

    增加类的注释说明

--- src/main/scala/org/brave/spark/base/BaseConf.scala
+++ src/main/scala/org/brave/spark/base/BaseConf.scala
@@ -11,12 +11,33 @@ import org.brave.util.PropertiesUtil
  *
  * spark运行的基本参数
  * 详细请参见：src/main/resources/util.properties
+ * 写到配置文件的好处：不需要编译
  */
 class BaseConf {
   var properties: Properties = PropertiesUtil.loadProperties("/util.properties")
+  /**
+   * 青云的集群URL
+   *
+   */
   var sparkMasterRemote: String = properties.getProperty("spark.master.remote")
+  /**
+   * 本地的集群URL
+   */
+  var sparkMasterRemoteLocal: String = properties.getProperty("spark.master.remote.local")
+  /**
+   * 本地local
+   */
   var sparkMasterLocal: String = properties.getProperty("spark.master.local")
+  /**
+   * spark运行内存可以是m为单位，也可以是以g为单位
+   */
   var sparkDriverMemory: String = properties.getProperty("spark.driver.memory")
+  /**
+   * 集群demo文件的地址
+   */
   var demoFilePathRemote: String = properties.getProperty("demo.file.path.remote")
+  /**
+   * 本地demo文件的地址
+   */
   var demoFilePathLocal: String = properties.getProperty("demo.file.path.local")
 }
