1388a07cd62ce20f3ab6c6edb7cd96c285054101
Qingyang <fyefyang@gmail.com>
2016-07-17 14:13:02 +0800

    pom

--- src/main/bash/org/brave/scripts/movietag.sh
+++ src/main/bash/org/brave/scripts/movietag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #这个脚本会对movies.txt做一个二次的清洗，只提取电影的标签字段。
 #to transform movies.txt to movietags.txt, to avoid the bad impact of ',' in the second field of movies.txt
-while read LINE
+    while read LINE
  do 
  echo ${LINE##*,} >> movietags.txt
 done < movies.txt
