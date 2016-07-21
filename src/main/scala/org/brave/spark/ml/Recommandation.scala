9fb7915cdd3e8a9befc0330ef25ece72d32b7f36
San Luo <luohui20001@sina.com>
2016-07-21 22:12:11 +0800

    完成了推荐系统原形的开发，已经能推荐出电影来了，参见开发日记。 加入了推荐程序的运行脚本

--- src/main/scala/org/brave/spark/ml/Recommandation.scala
+++ src/main/scala/org/brave/spark/ml/Recommandation.scala
@@ -1,61 +1,108 @@
 package org.brave.spark.ml
 
+import scala.util.Random
+
 import org.apache.spark._
 import org.apache.spark.mllib.recommendation.ALS
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.mllib.recommendation.Rating
+import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
 
 import org.brave.spark.base.BaseConf
+import org.brave.util.util._
 
 /**
  * 运行完ETL后可以运行这个类。这个是一个实验性质的类，
  * 取某个表的数据提取特征向量，然后用ALS来训练一个模型。
  * BASED ON https://github.com/apache/spark/blob/master/examples/src/main/scala/org/apache/spark/examples/mllib/RecommendationExample.scala
  */
-object Recommandation extends BaseConf {
+object Recommandation {
   def main(args: Array[String]) {
+    if (args.length < 2) {
+      System.err.print(s"""
+                          |Usage: Recommandation <inputUserId> <recommandResultNum>
+        """.stripMargin)
+      System.exit(1)
+    }
+    val inputUserId = args(0).trim().toInt
+    val recommandResultNum = args(1).trim().toInt
+
+    //随机产生一个userid和一个随机的推荐电影数量
+    val randomNumber1 = scala.util.Random.nextInt().%(234934)
+    val randomUserId = if (randomNumber1 > 0) randomNumber1 else -randomNumber1
+    val randomNumber2 = scala.util.Random.nextInt().%(5)
+    val randomResultNum = if (randomNumber2 > 0) randomNumber2 + 1 else -randomNumber2 + 1
+
+    val conf = new SparkConf().setMaster("spark://master:7077")
     conf.setAppName("Recommandation")
     val sc = new SparkContext(conf)
     val hc = new org.apache.spark.sql.hive.HiveContext(sc)
-    hc.sql("cache table movies")
     hc.sql("cache table ratings")
-    hc.sql("cache table links")
-    hc.sql("cache table tags")
+    hc.sql("cache table movies")
+    //    val allUsers = hc.sql("select distinct(userid) from ratings")
+    if (hc.sql(s"select userid from ratings where userid=$inputUserId").count() == 0) {
+      println("Invalid user id!")
+      println("User id ranges from 1 to 234934")
+      sc.stop()
+    }
 
     val ratings = hc.sql("select userid,movieid,rating from ratings")
     val ratingRDD = ratings.rdd.map(x =>
-      Array(x.getInt(0), x.getInt(1), x.getFloat(2).toDouble)).map {_ match {
+      Array(x.getInt(0), x.getInt(1), x.getFloat(2).toDouble)).map {
+      _ match {
         case Array(userid, movieid, rating) => Rating(userid.toInt, movieid.toInt, rating.toDouble)
       }
     }
+    ratingRDD.persist()
 
     val rank = 10
     val numIterations = 10
     val model = ALS.train(ratingRDD, rank, numIterations, 0.01)
-    
-    val userAndMovie = ratingRDD.map{
+    ratingRDD.unpersist(true)
+
+    //为输入的用户推荐
+    val recommandResult = model.recommendProducts(inputUserId, recommandResultNum)
+    val recommandMovies = recommandResult.map { x => x.product.toString() }
+    println(s"the recommanded movies for specified user NO.$inputUserId is :")
+    //    recommandMovies.foreach { println }
+    val recommandMovieIDs = recommandMovies.map(_.concat(",")).mkString
+    hc.sql(s"select title from movies where movieId in (${recommandMovieIDs.substring(0, recommandMovieIDs.length - 1)})").collect().foreach(println)
+    //为随机用户推荐
+    val randomRecommandResult = model.recommendProducts(randomUserId, randomResultNum)
+    val randomRecommandMovies = randomRecommandResult.map { x => x.product.toString() }
+    println(s"the recommanded movies for a random user NO.$randomUserId is :")
+    //    randomRecommandMovies.foreach { println }
+    val randomRecommandMovieIDs = randomRecommandMovies.map(_.concat(",")).mkString
+    hc.sql(s"select title from movies where movieId in (${randomRecommandMovieIDs.substring(0, randomRecommandMovieIDs.length - 1)})").collect().foreach(println)
+
+    hc.sql("uncache table ratings")
+    hc.sql("uncache table movies")
+
+    val userAndMovie = ratingRDD.map {
       case Rating(user, product, rate) => (user, product)
     }
-    
+
     val pred = model.predict(userAndMovie).map {
       case Rating(user, product, rate) => ((user, product), rate)
     }
 
-    val vectors = org.apache.spark.sql.functions.udf((movieId: Int, rating: Float) =>
-      Vectors.dense(movieId, rating))
-    val label = org.apache.spark.sql.functions.udf((rating: Float) =>
-      rating.toDouble)
-    val newratings = ratings.withColumn("featureV", vectors(ratings.col("movieId"), ratings.col("rating"))).withColumn("label", label(ratings.col("rating")))
-    newratings.printSchema()
-    newratings.registerTempTable("newratings")
-    hc.sql("select distinct(label) from newratings").collect.foreach(println)
+    val ratesAndPreds = ratingRDD.map {
+      case Rating(user, product, rate) => ((user, product), rate)
+    }.join(pred)
 
-    val lr = new LogisticRegression().setFeaturesCol("featureV").setLabelCol("label")
-    println("LogisticRegression parameters:\n" + lr.explainParams() + "\n")
-    lr.setMaxIter(10).setRegParam(0.01)
+    val MSE = ratesAndPreds.map {
+      case ((user, product), (r1, r2)) =>
+        val err = (r1 - r2)
+        err * err
+    }.mean()
+    println("Mean Squared Error = " + MSE)
 
-    val model1 = lr.fit(newratings)
-    println("Model 1 was fit using parameters: " + model1.parent.extractParamMap)
+    val ct = new CalendarTool
+    val today = ct.getToday
+
+    //模型的保存和加载
+    //    model.save(sc, s"model/myCollaborativeFilter$today")
+    //    val safedModel = MatrixFactorizationModel.load(sc, s"model/myCollaborativeFilter$today")
   }
 }
