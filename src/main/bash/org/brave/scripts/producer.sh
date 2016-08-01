6b059a3ba2c24350c6fdc7060552b01be557719f
San Luo <luohui20001@sina.com>
2016-08-01 18:22:05 +0800

    更新producer,已经完成读取DataExtractor所产生的streamingdata的HDFS文件，然后按照一定的速度把他打到kafka里。交由sparkstreaming接收这些数据来实时产生推荐结果。

---
+++ src/main/bash/org/brave/scripts/producer.sh
@@ -0,0 +1,3 @@
+
+#!/bin/bash
+$SPARK_HOME/bin/spark-submit --class org.brave.spark.streaming.Producer $SPARK_HOME/BraveFirst-0.0.1-SNAPSHOT-jar-with-dependencies.jar
--- src/main/scala/org/brave/spark/streaming/Producer.scala
+++ src/main/scala/org/brave/spark/streaming/Producer.scala
@@ -10,15 +10,17 @@ import org.brave.spark.base.BaseConf
 import org.brave.util.util.CalendarTool
 
 /*
+ * 运行此类之前，需要先运行DataExtractor类。
+ * 读取DataExtractor产生的ratings_streaming数据，把它按照一定的速率打入到kafka消息队列里。
  * 把producer通过spark-submit提交到Spark集群运行，即可正常发送消息。
  * 如果以java -jar的方式，会出现异常com.typesafe.config.ConfigException$Missing: No configuration setting found for key 'akka.version'
  * 
- */ 
+ */
 object Producer extends BaseConf {
   def main(args: Array[String]) {
     conf.setAppName("Producer")
     val sc = new SparkContext(conf)
-    val Array(brokers, topic, messagesPerBatch) = Array("master60:9092", "test", "200")
+    val Array(brokers, topic) = Array("master60:9092", "test")
     val props = new Properties()
     props.put("bootstrap.servers", "master60:9092")
     props.put("acks", "all")
@@ -32,23 +34,19 @@ object Producer extends BaseConf {
 
     val producer = new KafkaProducer[String, String](props)
     val file = sc.textFile("/data/ratings_streaming")
-    val recordNum = file.count().toInt
-    val batchNum = recordNum/messagesPerBatch.toInt
-    val fileP = file.repartition(batchNum)
-    val filePA = fileP.partitions
     val c = new CalendarTool
-    val arr1 = Array(0,1,2)
-    val e2 = arr1(2)
 
-    //正常应该是while(true)，让这个producer一直读取某个文件夹，并发送更新的记录到队列里
-    for(i<- 0 to batchNum){
-      val time = c.getCurrentTime
-      val p = filePA.apply(i).toString()
-      val str =time+p
-      val messages = new ProducerRecord[String, String](topic, str)
-      producer.send(messages)
-      println(messages)
-      Thread.sleep(messagesPerBatch.toLong)
+    while(true) {
+      val itr = file.toLocalIterator
+      while (itr.hasNext) {
+        val time = c.getCurrentTime
+        val r = itr.next()
+        val str = time + "\n" + r
+        val messages = new ProducerRecord[String, String](topic, str)
+        println(messages)
+        producer.send(messages)
+        Thread.sleep(batchInterval.toLong)
+      }
     }
   }
 }
